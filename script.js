fc849551d15a9760578608b56e4b831eaebacb72
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-08-31 22:40:06 +0500

    Initial Commit Project

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const createQuizBtn = document.getElementById("createQuizBtn");
+    const takeQuizBtn = document.getElementById("takeQuizBtn");
+
+    createQuizBtn.addEventListener("click", () => {
+        // Redirect to quiz creation page or open a modal
+    });
+
+    takeQuizBtn.addEventListener("click", () => {
+        
+        // Redirect to quiz listing page or open a modal
+    });
+});
